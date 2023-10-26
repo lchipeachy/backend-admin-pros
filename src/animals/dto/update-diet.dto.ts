59a558357a2cdc6e09eff8300e6344897ec40e14
Andrea Lucia <bvlucia34@gmail.com>
2023-10-26 00:09:26 -0600

    update diet

---
+++ src/animals/dto/update-diet.dto.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateDietDto } from './create-diet.dto';
+
+export class UpdateDietDto extends PartialType(CreateDietDto) {}
